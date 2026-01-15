ba72ce2a34f48fb059cce2484b3ab2a58aa1b2b6
Sushant Gawali <sushantgawali38@gmail.com>
2026-01-16 02:34:08 +0530

    Add files via upload

---
+++ 02_problem2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int r = 6;
+    int height = 10;
+
+    printf("The arera of circle with radius %d is %f\n", r, 3.14 * r * r);
+    printf("The volume of cyclinder with radius %d and Height %d is %f", r, height, 3.14 * r * r * height);
+    return 0;
+}
